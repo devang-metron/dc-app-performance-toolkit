437f80313b8be528b646a183f7c0413a24989926
Devang Mungara <devang@metronlabs.com>
2023-06-24 14:22:49 +0530

    Updated app specific actions

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -1,27 +1,31 @@
+import re
+from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
 import json
 import random
+import requests
 import re
+import urllib.parse
 from locustio.jira.requests_params import jira_datasets
 from locustio.common_utils import init_logger, jira_measure, RESOURCE_HEADERS, ADMIN_HEADERS, generate_random_string
 
 logger = init_logger(app_type='jira')
 jira_dataset = jira_datasets()
 
-
-
 @jira_measure("locust_app_specific_action")
 # @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    # Select Projecy 
+    # Select Project
+    ADMIN_HEADERS_LOCAL = dict(ADMIN_HEADERS)
+    ADMIN_HEADERS_LOCAL["Content-Type"] = 'application/json'
     project = random.choice(jira_dataset['projects'])
     project_id = project[1]
 
     # Get issue type for the project
-    response = locust.get(f'/rest/api/2/issue/createmeta?projectIds={project_id}', headers=RESOURCE_HEADERS, catch_response=True)
+    response = locust.get(f'/rest/api/2/issue/createmeta/{project_id}/issuetypes', headers=RESOURCE_HEADERS, catch_response=True)
     content = json.loads(response.content)
-    project_key = content["projects"][0]["key"] 
-    issue_type = content["projects"][0]["issuetypes"][0]["name"]
-    logger.locust_info(f"Project Key: {project_key}")
+    issue_type = content["values"][0]["name"]
+    if issue_type == "Epic" and len(content["values"]) > 1:
+        issue_type = content["values"][1]["name"]
     logger.locust_info(f"issue_type: {issue_type}")
     
     # Create issue in project
@@ -29,11 +33,12 @@ def app_specific_action(locust):
     description = f'Locust description {generate_random_string(10, only_letters=True)}'
     incident_id = f'IncidentId:1234567'
 
+    
     body = {
         "fields": {
             "project": {
-                "key": project_key
-                }, 
+                "id": project_id
+                },
             "summary": f"{summary}",
             "description": f"{description}",
             "priority": { "name": "High" },
@@ -41,6 +46,23 @@ def app_specific_action(locust):
             "issuetype": {"name": issue_type}
             }
         }
+        
     logger.locust_info(f"Json Body: {body}")
-    response = locust.post(f'/rest/api/2/issue', headers=ADMIN_HEADERS, json=body, catch_response=True)
+    logger.info(f"Json Body: {body}")
+    logger.info(f'admin headers :{ADMIN_HEADERS}')
+  
     
+    response = locust.post(f'/rest/api/2/issue', headers=ADMIN_HEADERS_LOCAL, json=body, catch_response=True)
+    
+    # adding issue property
+    response = response.json()
+    issue_key = response.get("key","")
+    logger.info('issue_key is : {}'.format(issue_key))
+
+    body = {
+        "id": "1234567"
+    }
+
+
+    response = requests.put(f'http://a0b4f280e1ef94a408498508c2775226-728806281.us-east-2.elb.amazonaws.com/jira/rest/api/2/issue/{issue_key}/properties/digitalshadows_incident', headers=ADMIN_HEADERS_LOCAL, json=body)# Select Project
+ 
